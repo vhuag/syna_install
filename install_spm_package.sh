dd720b18786f7f7f2d18beeab5b9a272ad56efab
Vincent Huang <vincenth@synaptics.com>
2023-07-13 11:02:10 +0800

    modify install

--- install_spm_package.sh
+++ install_spm_package.sh
@@ -43,8 +43,48 @@ TOKEN=$(curl -s $TOKEN_URL)
 
 OWNER="vhuag"
 REPO="spm"
-PATH_TO_FILE="package/$PACKAGE/$BIN_NAME"
+DIR_PATH="package/$PACKAGE"
+PATH_TO_FILE="$DIR_PATH/$BIN_NAME"
 FILE_NAME=$(basename $PATH_TO_FILE)
+
+
+# Get list of all files in the directory
+files_json=$(curl -H "Authorization: token $TOKEN" \
+  https://api.github.com/repos/$OWNER/$REPO/contents/$DIR_PATH)
+
+# Parse the JSON response to get an array of all the file paths
+file_paths=($(echo $files_json | jq -r '.[] | .path'))
+
+# Iterate over the array and download each file
+for file_path in "${file_paths[@]}"
+do
+  FILE_NAME=$(basename $file_path)
+  TEMP_FILE_NAME="_$FILE_NAME" # temporary filename
+  curl -f -H "Authorization: token $TOKEN" \
+    -H 'Accept: application/vnd.github.v3.raw' \
+    -o $TEMP_FILE_NAME \
+    -L https://api.github.com/repos/$OWNER/$REPO/contents/$file_path
+
+  # Check if the curl command was successful
+  if [ $? -eq 0 ]; then
+    echo "File $TEMP_FILE_NAME downloaded successfully"
+    # Check if the destination directory exists
+    if [ ! -d "$ROOT_DIR/$DIR_PATH" ]; then
+        sudo mkdir -p $ROOT_DIR/$DIR_PATH
+    fi
+    # Move and rename the file back to its original name
+    sudo mv $TEMP_FILE_NAME $ROOT_DIR/$file_path
+  else
+    echo "File download failed"
+    exit 1
+  fi
+done
+
+echo completed
+exit 1
+
+
+
 
 curl -f -H "Authorization: token $TOKEN" \
      -H 'Accept: application/vnd.github.v3.raw' \

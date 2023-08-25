d85467dc4a1b00f4a47ac2e188c1c78f4471475b
Vincent Huang <vincenth@synaptics.com>
2023-08-25 06:36:09 +0000

    Update installer version in rpi4_android_dev.sh to 1.2

--- rpi4_android_dev.sh
+++ rpi4_android_dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 INSTALLER_MAJOR="1"
-INSTALLER_MINOR="1"
+INSTALLER_MINOR="2"
 echo "RPI4 android dev installer version: $INSTALLER_MAJOR.$INSTALLER_MINOR"
 
 #function to install android development environment

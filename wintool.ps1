6687eebaa902baa6d69c66b59030897be8b1f03a
Vincent Huang <vincenth@synaptics.com>
2023-09-07 14:36:08 +0800

    try wintool

--- wintool.ps1
+++ wintool.ps1
@@ -1,19 +1,37 @@
 Write-Host "Windows tool installer"
 
 param(
-    [Parameter(ValueFromRemainingArguments = $true)]
+    [Parameter(Position=0)]
+    [string]$pythonFolderPath,
+
+    [Parameter(Position=1, ValueFromRemainingArguments = $true)]
     [string[]]$args
 )
-Write-Host "Windows tool installer with arguments: $args"
 
-# Download the script content
+# Add Python folder path to PATH environment variable for this session
+$env:Path = "$env:Path;$pythonFolderPath"
+
+# Create C:\spm directory if it doesn't exist
+$spmFolderPath = "C:\spm"
+if (-Not (Test-Path $spmFolderPath)) {
+    New-Item -Path $spmFolderPath -ItemType Directory
+}
+
+# Add C:\spm to PATH environment variable for this session
+$env:Path = "$env:Path;$spmFolderPath"
+
+# Download spm and spm.json from GitHub to C:\spm
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/vhuag/syna_install/master/spm' -OutFile "$spmFolderPath\spm"
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/vhuag/syna_install/master/spm.json' -OutFile "$spmFolderPath\spm.json"
+
+# Download the main script content
 $scriptContent = (Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/vhuag/syna_install/master/test.ps1').Content
 
 # Save the script content to a temporary file
 $tempFile = [System.IO.Path]::GetTempFileName()
 Set-Content -Path $tempFile -Value $scriptContent
 
-# Run the downloaded script with the provided arguments
+# Run the downloaded script with the remaining provided arguments
 & $tempFile @args
 
 # Optional: Remove the temporary file

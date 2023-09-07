599e55bfc1a0f42fa72a2a71ffb3bdd71762ba78
Vincent Huang <vincenth@synaptics.com>
2023-09-07 14:11:36 +0800

    add message

--- rpi4_android_dev.sh
+++ rpi4_android_dev.sh
@@ -23,6 +23,7 @@ function install_android_dev_env()
     curl -o ${REPO} https://storage.googleapis.com/git-repo-downloads/repo
     gpg --recv-keys 8BB9AD793E8E6153AF0F9A4416530D5E920F5C65
     curl -s https://storage.googleapis.com/git-repo-downloads/repo.asc | gpg --verify - ${REPO} && install -m 755 ${REPO} ~/bin/repo
+    echo 'We will close this script now, please restart this script to continue'
     #restart this shell to make sure path is set
     exec bash
 }

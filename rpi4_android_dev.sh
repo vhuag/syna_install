2eef5da054f7937a3fbd66e774994d77dd8e163d
Vincent Huang <vincenth@synaptics.com>
2023-09-07 14:09:27 +0800

    restart bash

--- rpi4_android_dev.sh
+++ rpi4_android_dev.sh
@@ -23,6 +23,8 @@ function install_android_dev_env()
     curl -o ${REPO} https://storage.googleapis.com/git-repo-downloads/repo
     gpg --recv-keys 8BB9AD793E8E6153AF0F9A4416530D5E920F5C65
     curl -s https://storage.googleapis.com/git-repo-downloads/repo.asc | gpg --verify - ${REPO} && install -m 755 ${REPO} ~/bin/repo
+    #restart this shell to make sure path is set
+    exec bash
 }
 
 #function to download android rpi4 source code
